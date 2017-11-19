4f782c45832f79bb3e4b293a3c1837b9de53002b
JunlingWang <junling-wang@hotmail.com>
2017-11-19 14:22:57 +1100

    Add English documentation

--- index.php
+++ index.php
@@ -7,9 +7,30 @@ PHP代码是运行在服务器端的，html和Javascript是运行在客户端的
 发给客户端浏览器。
 php不仅可以读取数据库，也可以对数据库进行修改、写入操作。
 要用户在浏览器端的操作（点击、输入等）编辑数据库，需要采用
-AJAX技术。首先用html事件调用Javascrip函数，后者再调用php文件操作
+AJAX技术。首先用html事件调用Javascript函数，后者再调用php文件操作
 数据库。php在除了操作数据库以外，还可以把获取的数据发送到Javascript
 指定的html标签中。
+PHP code runs on the server side, while html and Javascript run
+ on the client side. When a browser visits a .php file on a 
+ server, the php code in the file will be executed by the server,
+ and the server will generate html code accordingly.
+ 
+ However, Javascript code will not be processed by the server. It 
+ will be send to the client end together with the html code, and then
+  be executed by the browser.
+  
+ The operation of database can be done by PHP, and the result data
+ can be sent to client end in a form of html.
+ 
+HPH can be used not only to read the database, but also the writing
+ and revising of it.
+ 
+ If you want to operate the database via the browser, 
+ such as by clicking and inputing data, AJAX is needed. Firstly,
+ call Javascript functions with html events, such as a button click,
+ and then the Javascript function calls php function to operate the
+ database. The php function can then send the result data to a html
+ mark specified by Javascript.
 -->
 
 <?php
